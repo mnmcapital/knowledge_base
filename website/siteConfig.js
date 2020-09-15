dd4220e14f297b6d19437499acb1b16bf926d299
Misha Stone <misha@paypiip.com>
2020-09-15 14:57:40 -0400

    Mm.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
   url: 'https://mnmcapital.github.io', // Your website URL
-  baseUrl: '/knowledge_base', // Base URL for your project */
+  baseUrl: '/knowledge_base/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
